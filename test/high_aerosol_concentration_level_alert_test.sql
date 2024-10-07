b134ebea7a765bff1c27a1dbd99c467c98035e44
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-10-07 12:44:50 -0300

    refactor: use seq_id as default id & add single quotes to names

--- test/high_aerosol_concentration_level_alert_test.sql
+++ test/high_aerosol_concentration_level_alert_test.sql
@@ -1,5 +1,5 @@
-INSERT INTO ozone_monitoring_instruments (DEVICE_ID, AEROSOL_AOD, AREA) VALUES (seq_air_monitoring_device_id.nextval, 0.5, 'Paul´s Courtyard');
+INSERT INTO ozone_monitoring_instruments (AEROSOL_AOD, AREA) VALUES (0.5, 'Paul''s Courtyard');
 
-UPDATE ozone_monitoring_instruments SET AEROSOL_AOD = 0.8 WHERE AREA = 'Paul´s Courtyard';
+UPDATE ozone_monitoring_instruments SET AEROSOL_AOD = 0.8 WHERE AREA = 'Paul''s Courtyard';
 
 SELECT "LOG_ID", "DEVICE_ID", "FROM", "TO", "TEXT", "TIME" FROM air_monitoring_logs;
--- test/methane_explosion_alert_test.sql
+++ test/methane_explosion_alert_test.sql
@@ -1,5 +1,5 @@
-INSERT INTO gas_leak_detectors (DEVICE_ID, CH4_PPM, SITE) VALUES (seq_air_monitoring_device_id.nextval, 15000, 'Marie´s Courtyard');
+INSERT INTO gas_leak_detectors (CH4_PPM, SITE) VALUES (15000, 'Marie''s Courtyard');
 
-UPDATE gas_leak_detectors SET CH4_PPM = 60000 WHERE SITE = 'Marie´s Courtyard';
+UPDATE gas_leak_detectors SET CH4_PPM = 60000 WHERE SITE = 'Marie''s Courtyard';
 
 SELECT "LOG_ID", "DEVICE_ID", "FROM", "TO", "TEXT", "TIME" FROM air_monitoring_logs;
