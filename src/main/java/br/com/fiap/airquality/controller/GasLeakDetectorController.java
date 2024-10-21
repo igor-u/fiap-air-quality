f9060a44672907b190864a680ecb6801d1ec3180
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2024-10-21 13:19:36 -0300

    feat: post methods for gld & omi controllers

--- src/main/java/br/com/fiap/airquality/controller/GasLeakDetectorController.java
+++ src/main/java/br/com/fiap/airquality/controller/GasLeakDetectorController.java
@@ -1,7 +1,29 @@
 package br.com.fiap.airquality.controller;
 
+import br.com.fiap.airquality.model.device.impl.GasLeakDetector;
+import br.com.fiap.airquality.service.GasLeakDetectorService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("air_quality")
 public class GasLeakDetectorController {
+
+    private final GasLeakDetectorService gasLeakDetectorService;
+
+    @Autowired
+    public GasLeakDetectorController(GasLeakDetectorService gasLeakDetectorService) {
+        this.gasLeakDetectorService = gasLeakDetectorService;
+    }
+
+    @PostMapping("/gas_leak_detector")
+    @Transactional
+    public GasLeakDetector createDevice(@RequestBody GasLeakDetector gasLeakDetector) {
+        return gasLeakDetectorService.createDevice(gasLeakDetector);
+    }
+
 }
--- src/main/java/br/com/fiap/airquality/controller/OzoneMonitoringInstrumentController.java
+++ src/main/java/br/com/fiap/airquality/controller/OzoneMonitoringInstrumentController.java
@@ -1,7 +1,31 @@
 package br.com.fiap.airquality.controller;
 
+import br.com.fiap.airquality.model.device.impl.AirQualitySensor;
+import br.com.fiap.airquality.model.device.impl.OzoneMonitoringInstrument;
+import br.com.fiap.airquality.service.AirQualitySensorService;
+import br.com.fiap.airquality.service.OzoneMonitoringInstrumentService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("air_quality")
 public class OzoneMonitoringInstrumentController {
+
+    private final OzoneMonitoringInstrumentService ozoneMonitoringInstrumentService;
+
+    @Autowired
+    public OzoneMonitoringInstrumentController(OzoneMonitoringInstrumentService ozoneMonitoringInstrumentService) {
+        this.ozoneMonitoringInstrumentService = ozoneMonitoringInstrumentService;
+    }
+
+    @PostMapping("/ozone_monitoring_instrument")
+    @Transactional
+    public OzoneMonitoringInstrument createDevice(@RequestBody OzoneMonitoringInstrument ozoneMonitoringInstrument) {
+        return ozoneMonitoringInstrumentService.createDevice(ozoneMonitoringInstrument);
+    }
+
 }
