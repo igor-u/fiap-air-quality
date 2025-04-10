06321add76dc4b9a31574ec0b541956a0681f531
Igor Ribeiro <igor.ribeiro2334@gmail.com>
2025-04-09 21:37:18 -0300

    build: env host

--- src/main/java/br/com/fiap/airquality/config/mongo/MongoConfig.java
+++ src/main/java/br/com/fiap/airquality/config/mongo/MongoConfig.java
@@ -1,18 +1,36 @@
 package br.com.fiap.airquality.config.mongo;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.lang.NonNull;
 
+import static java.util.Collections.singletonList;
+
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        String mongoHost = env.getProperty("spring.data.mongodb.host");
+        builder
+                .applyToClusterSettings(settings  -> {
+                    settings.hosts(singletonList(new ServerAddress(mongoHost)));
+                });
     }
 
     @Override
